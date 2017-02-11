6fcb1b9b606c26944f6c393f1d93c8369ab1f0e7
daniign <daniign@gmail.com>
2017-02-11 10:37:09 -0500

    Add scraper module based on osmosis

---
+++ scraper/getdef.js
@@ -0,0 +1,14 @@
+const osmosis = require('osmosis')
+
+module.exports = (word) => {
+  return osmosis
+  .get(`https://es.wiktionary.org/wiki/${word}`)
+  .set({
+    'defs': ['dl'],
+    'thumbs':  ['img.thumbimage@src'],
+    'thumbIndexes': ['div.thumbcaption']
+  })
+//     .log(console.log)
+//     .error(console.log)
+//     .debug(console.log)
+}
---
+++ scraper/index.js
@@ -0,0 +1,14 @@
+const getdef = require('./getdef')
+
+// USE: 
+// getdef('tortuga')
+// .then((context, data) => {
+//   console.log(data)
+// })
+// .error((err) => {
+//   console.log('ERR:', err)
+// })
+
+module.exports = {
+  getdef
+}
