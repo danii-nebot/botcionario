33f2139adce227f0804d52f779f7364cbe0ca057
daniign <daniign@gmail.com>
2017-02-16 18:10:54 -0500

    Parse and map scraped data into Hero Cards

--- src/cards.js
+++ src/cards.js
@@ -1,32 +1,55 @@
+'use strict'
 const builder = require('botbuilder')
 
 const getHeroCards = (session, data, word) => {
   const cards = []
-  console.log(data)
+  const indexes = []
+  // Example dataset:
+//   const word= 'tortuga'
+//   const data = { 
+//     defs: [
+//       '1 Zoología.\nReptiles del orden de los Quelonios caracterizados por tener un tronco ancho y corto, protegido por un caparazón que está formado, generalmente, por placas óseas revestidas de placas córneas.\n\nHipónimo: galápago.',
+//       '2 Milicia.\nCubierta defensiva formada por las tropas levantando sus escudos.\n\nSinónimo: testudo.',
+//       '3\nLo que es muy lento.\n\nUso: coloquial, se aplica más a personas.',
+//       '4 Historia.\nAntiguo método de tortura en el cual el interrogado es tumbado en el suelo, poniéndole un tablón rectangular encima. En dicho tablón se coloca peso gradualmente, produciendo aplastamiento paulatino.',
+//       '1 Zoología.\nTortuga.'
+//     ],
+//     thumbs: [
+//       '//upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Egyptian_tortoise.JPG/220px-Egyptian_tortoise.JPG',
+//       '//upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Testudo_formation.jpg/220px-Testudo_formation.jpg',
+//       '//upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Giles_Corey.jpg/220px-Giles_Corey.jpg'
+//     ],
+//     thumbIndexes: [ '[1]', '[2]', '[4]' ] 
+//   }
+
   data.defs.forEach((def) => {
-    /* Example dataset:
-    { defs:
-   [ '1 Zoología.\nReptiles del orden de los Quelonios caracterizados por tener un tronco ancho y corto, protegido por un caparazón que está formado, generalmente, por placas óseas revestidas de placas córneas.\n\nHipónimo: galápago.',
-     '2 Milicia.\nCubierta defensiva formada por las tropas levantando sus escudos.\n\nSinónimo: testudo.',
-     '3\nLo que es muy lento.\n\nUso: coloquial, se aplica más a personas.',
-     '4 Historia.\nAntiguo método de tortura en el cual el interrogado es tumbado en el suelo, poniéndole un tablón rectangular encima. En dicho tablón se coloca peso gradualmente, produciendo aplastamiento paulatino.',
-     '1 Zoología.\nTortuga.' ],
-  thumbs:
-   [ '//upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Egyptian_tortoise.JPG/220px-Egyptian_tortoise.JPG',
-     '//upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Testudo_formation.jpg/220px-Testudo_formation.jpg',
-     '//upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Giles_Corey.jpg/220px-Giles_Corey.jpg' ],
-  thumbIndexes: [ '[1]', '[2]', '[4]' ] }
-    */
-    const [defText, auxText] = def.split('\n\n')
-    console.log(defText, auxText)
-    const card = new builder.HeroCard(session)
-      .title(word)
-      .subtitle(defText)
-      .text(defText)
-      .images([
-        builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
-      ])
-    cards.push(card)
+    const index = def[0] // we'll just ignore defs beyond the 9th
+    if (indexes.indexOf(index) === -1) {
+      indexes.push(index)
+      const context = def.substring(1, def.indexOf('\n'))
+      const defText = def.substring(def.indexOf('\n'))
+      let image
+      data.thumbIndexes.find((thumbIndex, i) => {
+        if (thumbIndex[1] === index) {
+          image = 'https:' + data.thumbs[i]
+          return true
+        }
+      })
+      
+      console.log(word)
+      console.log(`${index} ${context}`)
+      console.log(defText)
+      console.log(image)
+      
+      const card = new builder.HeroCard(session)
+        .title(word)
+        .subtitle(context)
+        .text(defText)
+        .images([
+          builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+        ])
+      cards.push(card)
+    }
   })
   return cards
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+'use strict'
 const restify = require('restify')
 const { connector } = require('./bot')
 
